a7714008515faf74f949feb9aae460ec8bfb845a
Kakooza Jerry Abraham <abrahamkakooza@gmail.com>
2021-04-21 20:58:08 +0300

    New commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+// Declaring a string variable
+let myName:String = "Jerry"
+
+// Declaring a number array
+let numberArray:number[] = [30,23]
+
+// Declaring a String array
+let stringArray:string[] = ['Name','Samson']
+
+let myBooleanArray:boolean[] = [true,false]
+
+let myObjArray:object[]=[{eric:"eric"}, [1,2,2]]
+
+// Declaring an object variable
+let newObject:object = {age:30,name:'Eric'}
+
+
+
